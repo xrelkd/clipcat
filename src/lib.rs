cbf4ea28795913a7d9c92802c8ee133fb1bf123a
xrelkd <46590321+xrelkd@users.noreply.github.com>
2021-02-24 22:10:21 +0800

    feat: implement std::str::FromStr for ClipboardMode

--- src/lib.rs
+++ src/lib.rs
@@ -77,6 +77,19 @@ impl ClipboardMode {
     }
 }
 
+impl FromStr for ClipboardMode {
+    type Err = ClipboardError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "clipboard" => Ok(ClipboardMode::Clipboard),
+            "selection" => Ok(ClipboardMode::Selection),
+            "primary" => Ok(ClipboardMode::Selection),
+            _ => Err(ClipboardError::ParseClipboardMode { value: s.to_string() }),
+        }
+    }
+}
+
 impl std::fmt::Display for ClipboardMode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.as_str())
