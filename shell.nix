a21b3f1f5ff2cf40f53b1e435d41bd7fee028c39
wrvsrx <wrvsrx@outlook.com>
2023-01-08 11:07:08 +0800

    Updated flake config

--- shell.nix
+++ shell.nix
@@ -1,7 +1,10 @@
-{ mkShell, clipcat }:
+{ mkShell, clipcat, clippy }:
 
 mkShell {
   inputsFrom = [ clipcat ];
+  buildInputs = [
+    clippy
+  ];
   # needed for internal protobuf c wrapper library
   inherit (clipcat)
     PROTOC
