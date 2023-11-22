6fe32689ee5c66664b56a5ceb24c370ed5289492
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-11-23 01:30:35 +0800

    feat: drop `rocksdb` (#39)

---
+++ crates/server/src/history/driver/mod.rs
@@ -0,0 +1,29 @@
+mod fs;
+
+use async_trait::async_trait;
+use clipcat::ClipEntry;
+
+pub use self::fs::FileSystemDriver;
+use crate::history::Error;
+
+#[async_trait]
+pub trait HistoryDriver: Send + Sync {
+    async fn load(&self) -> Result<Vec<ClipEntry>, Error>;
+
+    async fn save(&mut self, data: &[ClipEntry]) -> Result<(), Error>;
+
+    async fn clear(&mut self) -> Result<(), Error>;
+
+    async fn put(&mut self, data: &ClipEntry) -> Result<(), Error>;
+
+    async fn shrink_to(&mut self, min_capacity: usize) -> Result<(), Error>;
+
+    async fn save_and_shrink_to(
+        &mut self,
+        data: &[ClipEntry],
+        min_capacity: usize,
+    ) -> Result<(), Error> {
+        self.save(data).await?;
+        self.shrink_to(min_capacity).await
+    }
+}
--- crates/server/src/history/mod.rs
+++ crates/server/src/history/mod.rs
@@ -1,72 +1,57 @@
+mod driver;
 mod error;
-mod rocksdb;
 
 use std::path::{Path, PathBuf};
 
 use clipcat::ClipEntry;
 
-pub use self::{error::Error, rocksdb::RocksDBDriver};
-
-pub trait HistoryDriver: Send + Sync {
-    fn load(&self) -> Result<Vec<ClipEntry>, Error>;
-
-    fn save(&mut self, data: &[ClipEntry]) -> Result<(), Error>;
-
-    fn clear(&mut self) -> Result<(), Error>;
-
-    fn put(&mut self, data: &ClipEntry) -> Result<(), Error>;
-
-    fn get(&self, id: u64) -> Result<Option<ClipEntry>, Error>;
-
-    fn shrink_to(&mut self, min_capacity: usize) -> Result<(), Error>;
-
-    fn save_and_shrink_to(&mut self, data: &[ClipEntry], min_capacity: usize) -> Result<(), Error> {
-        self.save(data)?;
-        self.shrink_to(min_capacity)
-    }
-}
+pub use self::error::Error;
 
 pub struct HistoryManager {
     file_path: PathBuf,
-    driver: Box<dyn HistoryDriver>,
+    driver: Box<dyn driver::HistoryDriver>,
 }
 
 impl HistoryManager {
+    /// # Errors
     #[inline]
-    pub fn new<P: AsRef<Path>>(file_path: P) -> Result<Self, Error> {
-        let driver = Box::new(RocksDBDriver::open(&file_path)?);
+    pub fn new<P: AsRef<Path>>(file_path: P) -> Self {
         let file_path = file_path.as_ref().to_owned();
-        Ok(Self { file_path, driver })
+        let driver = Box::new(driver::FileSystemDriver::new(&file_path));
+        Self { file_path, driver }
     }
 
     #[inline]
     pub fn path(&self) -> &Path { &self.file_path }
 
     #[inline]
-    pub fn put(&mut self, data: &ClipEntry) -> Result<(), Error> { self.driver.put(data) }
-
-    #[inline]
-    #[allow(dead_code)]
-    pub fn clear(&mut self) -> Result<(), Error> { self.driver.clear() }
-
-    #[inline]
-    pub fn load(&self) -> Result<Vec<ClipEntry>, Error> { self.driver.load() }
-
-    #[inline]
-    pub fn save(&mut self, data: &[ClipEntry]) -> Result<(), Error> { self.driver.save(data) }
-
-    #[inline]
-    pub fn shrink_to(&mut self, min_capacity: usize) -> Result<(), Error> {
-        self.driver.shrink_to(min_capacity)
+    pub async fn put(&mut self, data: &ClipEntry) -> Result<(), Error> {
+        self.driver.put(data).await
     }
 
     #[inline]
-    pub fn save_and_shrink_to(
+    #[allow(dead_code)]
+    pub async fn clear(&mut self) -> Result<(), Error> { self.driver.clear().await }
+
+    #[inline]
+    pub async fn load(&self) -> Result<Vec<ClipEntry>, Error> { self.driver.load().await }
+
+    #[inline]
+    pub async fn save(&mut self, data: &[ClipEntry]) -> Result<(), Error> {
+        self.driver.save(data).await
+    }
+
+    #[inline]
+    pub async fn shrink_to(&mut self, min_capacity: usize) -> Result<(), Error> {
+        self.driver.shrink_to(min_capacity).await
+    }
+
+    #[inline]
+    pub async fn save_and_shrink_to(
         &mut self,
         data: &[ClipEntry],
         min_capacity: usize,
     ) -> Result<(), Error> {
-        self.save(data)?;
-        self.shrink_to(min_capacity)
+        self.driver.save_and_shrink_to(data, min_capacity).await
     }
 }
