54138d41f64ba727e5102e479c5401048919d8c6
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-11-23 13:41:56 +0800

    refactor(clipcat-notify): rename `ClipboardDriver` to `ClipboardBackend`

--- clipcat-notify/src/error.rs
+++ clipcat-notify/src/error.rs
@@ -6,14 +6,14 @@ pub enum Error {
     #[snafu(display("Could not initialize tokio Runtime, error: {source}"))]
     InitializeTokioRuntime { source: std::io::Error },
 
-    #[snafu(display("Could not create ClipboardDriver, error: {source}"))]
-    InitializeClipboardDriver { source: clipcat_server::clipboard_driver::Error },
+    #[snafu(display("Could not create clipboard backend, error: {source}"))]
+    InitializeClipboardBackend { source: clipcat_server::backend::Error },
 
     #[snafu(display("Could not wait for clipboard event"))]
     WaitForClipboardEvent,
 
     #[snafu(display("Could not subscribe clipboard event, error: {source}"))]
-    SubscribeClipboard { source: clipcat_server::clipboard_driver::Error },
+    SubscribeClipboard { source: clipcat_server::backend::Error },
 
     #[snafu(display("Nothing to listen"))]
     ListenNothing,
