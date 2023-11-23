49a06ad4918797f131925921eba147a36ff10bb6
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-11-24 00:33:41 +0800

    feat(crates/clipboard): rewrite `examples/store.rs`

--- crates/clipboard/examples/store.rs
+++ crates/clipboard/examples/store.rs
@@ -8,32 +8,69 @@ use std::{
 
 use clipcat::ClipboardContent;
 use clipcat_clipboard::{Clipboard, ClipboardKind, ClipboardStore};
+use sigfinn::{ExitStatus, LifecycleManager};
 use snafu::ErrorCompat;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    init_tracing();
     let clipboard = Clipboard::new(None, ClipboardKind::Clipboard)?;
     let data = format!("{:?}", Instant::now());
-    match clipboard.store(ClipboardContent::Plaintext(data.clone())) {
-        Ok(()) => {
-            println!("Press Ctrl-C to stop providing text: {data}");
-            println!("You can try to paste text into other window");
-            let term = Arc::new(AtomicBool::new(false));
-            let _ = signal_hook::flag::register(signal_hook::consts::SIGTERM, Arc::clone(&term))?;
-            let _ = signal_hook::flag::register(signal_hook::consts::SIGINT, Arc::clone(&term))?;
 
-            while !term.load(Ordering::Relaxed) {
-                std::thread::sleep(Duration::from_millis(100));
-            }
+    let lifecycle_manager = LifecycleManager::<clipcat_clipboard::Error>::new();
+    println!("Press Ctrl-C to stop providing text: {data}");
+    println!("You can try to paste text into other window");
 
-            println!("Exit");
-            Ok(())
-        }
-        Err(err) => {
-            eprintln!("{err}");
-            if let Some(backtrace) = ErrorCompat::backtrace(&err) {
-                eprintln!("{backtrace}");
+    let _handle = lifecycle_manager.spawn("Clipboard", |signal| async {
+        let term = Arc::new(AtomicBool::new(false));
+        let join_handle = tokio::task::spawn_blocking({
+            let term = term.clone();
+            move || match clipboard.store(ClipboardContent::Plaintext(data.clone())) {
+                Ok(()) => {
+                    while !term.load(Ordering::Relaxed) {
+                        std::thread::sleep(Duration::from_millis(100));
+                    }
+
+                    println!("Exit");
+                    drop(clipboard);
+                    Ok(())
+                }
+                Err(err) => {
+                    eprintln!("{err}");
+                    if let Some(backtrace) = ErrorCompat::backtrace(&err) {
+                        eprintln!("{backtrace}");
+                    }
+                    Err(err)
+                }
             }
-            Err(err)?
+        });
+
+        signal.await;
+        term.store(true, Ordering::Relaxed);
+
+        if let Err(err) = join_handle.await.expect("task is joinable") {
+            ExitStatus::Failure(err)
+        } else {
+            ExitStatus::Success
         }
-    }
+    });
+
+    tracing::info!("Press `Ctrl+C` to stop");
+    tracing::info!("Use `$ kill -s TERM {}` to stop", std::process::id());
+
+    drop(lifecycle_manager.serve().await?);
+    Ok(())
+}
+
+fn init_tracing() {
+    // filter
+    let filter_layer = tracing_subscriber::filter::LevelFilter::from_level(tracing::Level::INFO);
+
+    // format
+    let fmt_layer =
+        tracing_subscriber::fmt::layer().pretty().with_thread_ids(true).with_thread_names(true);
+
+    // subscriber
+    tracing_subscriber::registry().with(filter_layer).with(fmt_layer).init();
 }
