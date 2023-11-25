74909f6770710a221c4874ed6ea79009655a07f8
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-11-25 19:53:04 +0800

    build: bump version to 0.6.2

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
   outputs = { self, nixpkgs, flake-utils, fenix, crane }:
     let
       name = "clipcat";
-      version = "0.6.1";
+      version = "0.6.2";
     in
     (flake-utils.lib.eachDefaultSystem
       (system:
