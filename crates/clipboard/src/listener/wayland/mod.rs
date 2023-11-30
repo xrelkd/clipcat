f13066436cc6e1e963451ddb6649ea8c42d9fa50
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-11-30 18:48:12 +0800

    refactor(crates/clipboard): refactor

--- crates/clipboard/src/listener/wayland/mod.rs
+++ crates/clipboard/src/listener/wayland/mod.rs
@@ -48,50 +48,7 @@ impl Listener {
             }
         }
 
-        let thread = thread::spawn({
-            let is_running = is_running.clone();
-            move || {
-                while is_running.load(Ordering::Relaxed) {
-                    tracing::trace!("Wait for readiness events");
-
-                    match wl_clipboard_get_contents(
-                        clipboard_type,
-                        Seat::Unspecified,
-                        MimeType::Any,
-                    ) {
-                        Ok((_pipe, _mime_type)) => {
-                            notifier.notify_all();
-                            continue;
-                        }
-                        Err(
-                            WaylandError::NoSeats
-                            | WaylandError::ClipboardEmpty
-                            | WaylandError::NoMimeType,
-                        ) => {
-                            tracing::trace!("The clipboard is empty, sleep for a while");
-                        }
-                        Err(WaylandError::MissingProtocol { name, version }) => {
-                            tracing::error!(
-                                "A required Wayland protocol (name: {name}, version: {version}) \
-                                 is not supported by the compositor"
-                            );
-                        }
-                        Err(err) => {
-                            tracing::warn!(
-                                "Error occurs while listening to clipboard of Wayland, error: \
-                                 {err}"
-                            );
-                        }
-                    }
-                    // sleep for a while there is no content or error occurred
-                    thread::sleep(POLLING_INTERVAL);
-                }
-
-                notifier.close();
-                Ok(())
-            }
-        });
-
+        let thread = build_thread(is_running.clone(), clipboard_type, notifier);
         Ok(Self { is_running, thread: Some(thread), subscriber })
     }
 }
@@ -109,4 +66,45 @@ impl Drop for Listener {
         tracing::info!("Reap thread which listening to Wayland server");
         drop(self.thread.take().map(thread::JoinHandle::join));
     }
+}
+
+#[allow(clippy::cognitive_complexity)]
+fn build_thread(
+    is_running: Arc<AtomicBool>,
+    clipboard_type: wl_clipboard_rs::paste::ClipboardType,
+    notifier: pubsub::Publisher,
+) -> thread::JoinHandle<Result<(), Error>> {
+    thread::spawn(move || {
+        while is_running.load(Ordering::Relaxed) {
+            tracing::trace!("Wait for readiness events");
+
+            match wl_clipboard_get_contents(clipboard_type, Seat::Unspecified, MimeType::Any) {
+                Ok((_pipe, _mime_type)) => {
+                    notifier.notify_all();
+                    continue;
+                }
+                Err(
+                    WaylandError::NoSeats | WaylandError::ClipboardEmpty | WaylandError::NoMimeType,
+                ) => {
+                    tracing::trace!("The clipboard is empty, sleep for a while");
+                }
+                Err(WaylandError::MissingProtocol { name, version }) => {
+                    tracing::error!(
+                        "A required Wayland protocol (name: {name}, version: {version}) is not \
+                         supported by the compositor"
+                    );
+                }
+                Err(err) => {
+                    tracing::warn!(
+                        "Error occurs while listening to clipboard of Wayland, error: {err}"
+                    );
+                }
+            }
+            // sleep for a while there is no content or error occurred
+            thread::sleep(POLLING_INTERVAL);
+        }
+
+        notifier.close();
+        Ok(())
+    })
 }
