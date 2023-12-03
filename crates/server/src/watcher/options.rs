78b55e40cf3fb8170af55fd5af4d8aebe3be333b
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-12-03 16:32:54 +0800

    feat(crates/server): filter clip content size

--- crates/server/src/watcher/options.rs
+++ crates/server/src/watcher/options.rs
@@ -7,15 +7,22 @@ pub struct Options {
     pub enable_primary: bool,
 
     pub filter_min_size: usize,
+
+    pub filter_max_size: usize,
 }
 
 impl Default for Options {
     fn default() -> Self {
         Self {
             load_current: true,
+
             enable_clipboard: true,
+
             enable_primary: true,
+
             filter_min_size: 1,
+            // 5 MiB
+            filter_max_size: 5 * (1 << 20),
         }
     }
 }
