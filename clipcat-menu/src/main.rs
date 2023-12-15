37f3a39962a0960d02d3a6fb848cc5038942c69d
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-12-16 05:52:46 +0800

    feat(treewide): remove `mimalloc`

--- clipcat-menu/src/main.rs
+++ clipcat-menu/src/main.rs
@@ -3,12 +3,7 @@ mod config;
 mod error;
 mod finder;
 
-use mimalloc::MiMalloc;
-
 use self::cli::Cli;
-
-#[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
 
 fn main() {
     if let Err(err) = Cli::default().run() {
