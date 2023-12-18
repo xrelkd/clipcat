8dc4a0ac22c0dbfd32f88567ed94f445db779cf5
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-12-18 21:16:43 +0800

    feat(crates/client): support gRPC authentication mechanisms

--- crates/client/src/watcher.rs
+++ crates/client/src/watcher.rs
@@ -22,38 +22,42 @@ pub trait Watcher {
 #[async_trait]
 impl Watcher for Client {
     async fn enable_watcher(&self) -> Result<ClipboardWatcherState, EnableWatcherError> {
-        let proto::WatcherStateReply { state } = proto::WatcherClient::new(self.channel.clone())
-            .enable_watcher(Request::new(()))
-            .await
-            .map_err(|source| EnableWatcherError::Status { source })?
-            .into_inner();
+        let proto::WatcherStateReply { state } =
+            proto::WatcherClient::with_interceptor(self.channel.clone(), self.interceptor.clone())
+                .enable_watcher(Request::new(()))
+                .await
+                .map_err(|source| EnableWatcherError::Status { source })?
+                .into_inner();
         Ok(state.into())
     }
 
     async fn disable_watcher(&self) -> Result<ClipboardWatcherState, DisableWatcherError> {
-        let proto::WatcherStateReply { state } = proto::WatcherClient::new(self.channel.clone())
-            .disable_watcher(Request::new(()))
-            .await
-            .map_err(|source| DisableWatcherError::Status { source })?
-            .into_inner();
+        let proto::WatcherStateReply { state } =
+            proto::WatcherClient::with_interceptor(self.channel.clone(), self.interceptor.clone())
+                .disable_watcher(Request::new(()))
+                .await
+                .map_err(|source| DisableWatcherError::Status { source })?
+                .into_inner();
         Ok(state.into())
     }
 
     async fn toggle_watcher(&self) -> Result<ClipboardWatcherState, ToggleWatcherError> {
-        let proto::WatcherStateReply { state } = proto::WatcherClient::new(self.channel.clone())
-            .toggle_watcher(Request::new(()))
-            .await
-            .map_err(|source| ToggleWatcherError::Status { source })?
-            .into_inner();
+        let proto::WatcherStateReply { state } =
+            proto::WatcherClient::with_interceptor(self.channel.clone(), self.interceptor.clone())
+                .toggle_watcher(Request::new(()))
+                .await
+                .map_err(|source| ToggleWatcherError::Status { source })?
+                .into_inner();
         Ok(state.into())
     }
 
     async fn get_watcher_state(&self) -> Result<ClipboardWatcherState, GetWatcherStateError> {
-        let proto::WatcherStateReply { state } = proto::WatcherClient::new(self.channel.clone())
-            .get_watcher_state(Request::new(()))
-            .await
-            .map_err(|source| GetWatcherStateError::Status { source })?
-            .into_inner();
+        let proto::WatcherStateReply { state } =
+            proto::WatcherClient::with_interceptor(self.channel.clone(), self.interceptor.clone())
+                .get_watcher_state(Request::new(()))
+                .await
+                .map_err(|source| GetWatcherStateError::Status { source })?
+                .into_inner();
         Ok(state.into())
     }
 }
