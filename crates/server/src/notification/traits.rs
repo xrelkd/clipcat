d0361e68da6acf8cbaac17a6fff7abde57caad93
xrelkd <46590321+xrelkd@users.noreply.github.com>
2024-10-26 10:35:16 +0800

    feat(crates/server): remove `Wayland` support

--- crates/server/src/notification/traits.rs
+++ crates/server/src/notification/traits.rs
@@ -20,10 +20,4 @@ pub trait Notification: Send + Sync {
         C: fmt::Display,
     {
     }
-
-    fn on_wayland_connected<C>(&self, _clipboard_kind: ClipboardKind, _connection_info: C)
-    where
-        C: fmt::Display,
-    {
-    }
 }
