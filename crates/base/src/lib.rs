54751061cf16de94bc5a2c7bc51ab8e08312e3cd
sandroid <sandroid@posteo.net>
2026-04-16 22:23:01 +0200

    feat(crates/base): add history dbus object

--- crates/base/src/lib.rs
+++ crates/base/src/lib.rs
@@ -31,6 +31,7 @@ pub const DBUS_OBJECT_PATH_PREFIX: &str = "/org/clipcat/clipcat";
 pub const DBUS_SYSTEM_OBJECT_PATH: &str = "/org/clipcat/clipcat/system";
 pub const DBUS_WATCHER_OBJECT_PATH: &str = "/org/clipcat/clipcat/watcher";
 pub const DBUS_MANAGER_OBJECT_PATH: &str = "/org/clipcat/clipcat/manager";
+pub const DBUS_HISTORY_OBJECT_PATH: &str = "/org/clipcat/clipcat/history";
 
 pub static PROJECT_SEMVER: LazyLock<semver::Version> = LazyLock::new(|| {
     semver::Version::parse(PROJECT_VERSION).unwrap_or(semver::Version {
