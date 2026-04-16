6433edf4c39477406984d980633bcf13db22bced
sandroid <sandroid@posteo.net>
2026-04-16 22:22:12 +0200

    feat(crates/server): support clearing the history

--- crates/server/src/grpc/mod.rs
+++ crates/server/src/grpc/mod.rs
@@ -1,9 +1,10 @@
+mod history;
 mod interceptor;
 mod manager;
 mod system;
 mod watcher;
 
 pub use self::{
-    interceptor::Interceptor, manager::ManagerService, system::SystemService,
-    watcher::WatcherService,
+    history::HistoryService, interceptor::Interceptor, manager::ManagerService,
+    system::SystemService, watcher::WatcherService,
 };
